6a30578c9c6621952eccedfb9e7f6253b0751082
playerdecuple <playerdecuple@gmail.com>
2020-12-12 07:33:53 +0900

    Test print player list

--- src/test/java/com/practicekingdecuple/TestMain.java
+++ src/test/java/com/practicekingdecuple/TestMain.java
@@ -1,11 +1,12 @@
 package com.practicekingdecuple;
 
 import com.developerdecuple.core.CardReader;
+import com.developerdecuple.core.PlayerReader;
 
 public class TestMain {
 
     public static void main(String[] args) {
-        CardReader.printAllCardInfo();
+        PlayerReader.printAllPlayer();
     }
 
 }
