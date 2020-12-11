5ff077314e34a392d6a856e9e393c3be3d19e35a
playerdecuple <playerdecuple@gmail.com>
2020-12-12 06:42:07 +0900

    Test card reader

---
+++ src/test/java/com/practicekingdecuple/TestMain.java
@@ -0,0 +1,11 @@
+package com.practicekingdecuple;
+
+import com.developerdecuple.core.CardReader;
+
+public class TestMain {
+
+    public static void main(String[] args) {
+        CardReader.printAllCardInfo();
+    }
+
+}
