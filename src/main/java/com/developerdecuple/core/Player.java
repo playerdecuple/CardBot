09a40a2b362cdd58ff4b6af0d81c23b3d20fdc41
playerdecuple <playerdecuple@gmail.com>
2020-12-12 09:34:15 +0900

    Update player information insert helper method

--- src/main/java/com/developerdecuple/core/Player.java
+++ src/main/java/com/developerdecuple/core/Player.java
@@ -1,11 +1,18 @@
 package com.developerdecuple.core;
 
+import com.developerdecuple.Main;
+
+import java.io.File;
+
 public class Player {
 
     private final long id;
     private final String name;
     private int level;
     private long exp, gold, cash;
+
+    private int backupLevel;
+    private long backupEXP, backupGold, backupCash;
 
     public Player(long id, String name, int level, long exp, long gold, long cash) {
         this.id = id;
@@ -14,6 +21,14 @@ public class Player {
         this.exp = exp;
         this.gold = gold;
         this.cash = cash;
+    }
+
+    private String getPlayerInfoForSVCFormat() {
+        return id + "," + name + "," + level + "," + exp + "," + gold + "," + cash + ",0";
+    }
+
+    private String getPlayerInfoForSVCFormat(long id, String name, int level, long exp, long gold, long cash) {
+        return id + "," + name + "," + level + "," + exp + "," + gold + "," + cash + ",0";
     }
 
     public long getId() {
@@ -41,18 +56,53 @@ public class Player {
     }
 
     public void setCash(boolean add, long cash) {
+        backupCash = this.cash;
         this.cash = add ? this.cash + cash : cash;
+        applyPlayerInfoIntoPlayerLists();
     }
 
     public void setExp(boolean add, long exp) {
+        backupEXP = this.exp;
         this.exp = add ? this.exp + exp : exp;
+        applyPlayerInfoIntoPlayerLists();
     }
 
     public void setGold(long gold) {
+        backupGold = this.gold;
         this.gold = gold;
+        applyPlayerInfoIntoPlayerLists();
     }
 
     public void setLevel(boolean add, int level) {
+        backupLevel = this.level;
         this.level = add ? this.level + level : level;
+        applyPlayerInfoIntoPlayerLists();
     }
+
+    public boolean existsInPlayerLists() {
+        String playerInfo = getPlayerInfoForSVCFormat();
+        File playerListFile = new File(Main.BASIC_PATH + "/PlayerList.txt");
+
+        if (!playerListFile.exists()) return false;
+        String playerListStr = new ReadFile().readString(playerListFile);
+
+        return playerListStr != null && playerListStr.contains(playerInfo);
+    }
+
+    public void applyPlayerInfoIntoPlayerLists() {
+        File playerListFile = new File(Main.BASIC_PATH + "/PlayerList.txt");
+
+        if (existsInPlayerLists()) {
+            String playerListStr = new ReadFile().readString(playerListFile);
+            String result = playerListStr != null ? playerListStr.replace(getPlayerInfoForSVCFormat(id, name, backupLevel, backupEXP, backupGold, backupCash), getPlayerInfoForSVCFormat()) : "";
+            new WriteFile().writeString(playerListFile, result);
+        } else {
+            String playerListStr = playerListFile.exists() ? new ReadFile().readString(playerListFile) : "";
+
+            assert playerListStr != null;
+            String result = playerListStr.equals("") ? getPlayerInfoForSVCFormat() : "\n" + getPlayerInfoForSVCFormat();
+            new WriteFile().writeString(playerListFile, result);
+        }
+    }
+
 }
