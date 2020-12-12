603d69814ab62f6adedabb66aeef36fa018f1146
playerdecuple <playerdecuple@gmail.com>
2020-12-12 09:40:10 +0900

    Update event listener(JDA)

---
+++ src/main/java/com/developerdecuple/Listener.java
@@ -0,0 +1,14 @@
+package com.developerdecuple;
+
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+
+public class Listener extends ListenerAdapter {
+
+    @Override
+    public void onGuildMessageReceived(@NotNull GuildMessageReceivedEvent ev) {
+        
+    }
+
+}
