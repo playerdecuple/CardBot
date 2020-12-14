3e2503f7f6cf1da74819a51e58f42ea58c88c652
playerdecuple <playerdecuple@gmail.com>
2020-12-14 11:26:09 +0900

    TODO!

---
+++ src/main/java/com/developerdecuple/battle/BattleCardSetting.java
@@ -0,0 +1,11 @@
+package com.developerdecuple.battle;
+
+public class BattleCardSetting {
+
+    public static void battleCardSet() {
+
+        // TODO : 배틀 카드를 세팅(battle.txt)하고 이걸로 배팅할수 있도록 만듬
+
+    }
+
+}
