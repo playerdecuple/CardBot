6d6700fc860e5c6191178da6892236d29b557e9a
playerdecuple <playerdecuple@gmail.com>
2020-12-23 13:53:31 +0900

    Update 'isBattleCard' method

--- src/main/java/com/developerdecuple/core/Card.java
+++ src/main/java/com/developerdecuple/core/Card.java
@@ -7,20 +7,38 @@ import java.util.Arrays;
 
 public class Card {
 
+    private int customId = -1;
     private final int id;
     private final String name;
     private final int star;
     private int atk;
     private int def;
     private final String description;
+    private boolean battleCard;
 
-    public Card(int id, String name, int star, int atk, int def, String description) {
+    public Card(int id, String name, int star, int atk, int def, String description, boolean battleCard) {
         this.id = id;
         this.name = name;
         this.star = star;
         this.atk = atk;
         this.def = def;
         this.description = description;
+        this.battleCard = battleCard;
+    }
+
+    public Card(int customId, int id, String name, int star, int atk, int def, String description, boolean battleCard) {
+        this.customId = customId;
+        this.id = id;
+        this.name = name;
+        this.star = star;
+        this.atk = atk;
+        this.def = def;
+        this.description = description;
+        this.battleCard = battleCard;
+    }
+
+    public boolean isBattleCard() {
+        return battleCard;
     }
 
     public int getId() {
@@ -55,13 +73,21 @@ public class Card {
         def = add ? def + amount : amount;
     }
 
+    public void setBattleCard(boolean battleCard) {
+        this.battleCard = battleCard;
+    }
+
+    public void setCustomId(int id) {
+        this.customId = id;
+    }
+
     public String getCardInfoForSVCFormat() {
-        String[] info = {String.valueOf(id), name, String.valueOf(star), String.valueOf(atk), String.valueOf(def), description};
-        return String.join(",", Arrays.copyOfRange(info, 0, info.length));
+        String[] info = {String.valueOf(id), name, String.valueOf(star), String.valueOf(atk), String.valueOf(def), description, battleCard ? "true" : "false"};
+        return (customId != -1 ? customId + "," : "") + String.join(",", Arrays.copyOfRange(info, 0, info.length));
     }
 
     public String toString(boolean msg) {
-        return msg ? name + "(★" + star + ") / ATK " + atk + ", DEF " + def + " / " + description : id + name + "(★" + star + ") / ATK " + atk + ", DEF " + def + " / " + description;
+        return msg ? name + "(★" + star + ") / ATK " + atk + ", DEF " + def + " / " + description + (battleCard ? " (♠)" : "") : id + name + "(★" + star + ") / ATK " + atk + ", DEF " + def + " / " + description + (battleCard ? " (♠)" : "");
     }
 
     public void saveDeck(String id) {
@@ -70,7 +96,7 @@ public class Card {
         if (!deckFile.exists()) return;
 
         String decks = new ReadFile().readString(deckFile);
-        String result = decks == null || decks.equals("") ? cardInfo : decks + "\n" + cardInfo;
+        String result = decks == null || decks.equals("") ? (customId != -1 ? customId + "," : "") + cardInfo : (customId != -1 ? customId + "," : "") + decks + "\n" + cardInfo;
 
         new WriteFile().writeString(deckFile, result);
     }
